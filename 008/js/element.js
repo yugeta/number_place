d0e68d0a75cc60089340e662abd0a3ea5b719133
Yugeta Koji <yugeta@myntinc.com>
2023-05-03 19:40:54 +0900

    add 008

---
+++ 008/js/element.js
@@ -0,0 +1,15 @@
+import { Main } from '../main.js'
+
+export class Element{
+  static get table(){
+    return document.getElementById(Main.stage_id)
+  }
+
+  static get tr_lists(){
+    return this.table.getElementsByTagName('tr')
+  }
+
+  static get elm_button(){
+    return document.querySelector('button#btn')
+  }
+}
